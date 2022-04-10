fe5c3908b84c30bd3fa29abdcb2ca0b919bf9a32
s23210 <73021331+s23210@users.noreply.github.com>
2022-04-10 11:42:47 +0200

    Update z12.php

--- z12.php
+++ z12.php
@@ -10,12 +10,7 @@
 <?php
 $tablica = array();
 $a = 4;
-$b = 4;
-
-if ($a != $b) {
-    echo "BŁĄD";
-    return;
-}
+$b = 10;
 
 for ($i = 0; $i < $a; $i++) {
     for ($j = 0; $j < $b; $j++) {
@@ -33,9 +28,16 @@ for ($i = 0; $i < $a; $i++) {
 
 echo "\n\n\n\n";
 
+$tablica2 = array();
 for ($i = 0; $i < $a; $i++) {
     for ($j = 0; $j < $b; $j++) {
-        echo $tablica[$j][$i] . " ";
+        $tablica2[$j][$i] = $tablica[$i][$j];
+    }
+    echo "\n";
+}
+for ($i = 0; $i < $b; $i++) {
+    for ($j = 0; $j < $a; $j++) {
+        echo $tablica2[$i][$j] . " ";
     }
     echo "\n";
 }
